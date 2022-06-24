ed636bc651c0952000be248993f811b92cdebfe5
Pablo Sreih <88791435+pablo-sreih@users.noreply.github.com>
2022-06-25 01:38:10 +0300

    Updated Contact Schema

--- Backend/model/contact.js
+++ Backend/model/contact.js
@@ -2,25 +2,11 @@ const mongoose = require("mongoose");
 
 
 const contactSchema = new mongoose.Schema({
-    name: {
-        type: String, required: true
-    },
-
-    email: {
-        type: String, default:null
-    },
-
-    number: {
-        type: Number, required: true, unique: true
-    },
-
-    status: {
-        type: String, default:null
-    },
-
-    location: {
-        type: [String], default:null
-    }
+    name: {type: String, required: true},
+    email: {type: String, default:null},
+    number: {type: Number, required: true, unique: true},
+    status: {type: String, default:null},
+    location: {type: [String], default:null}
 });
 
 module.exports = mongoose.model("contact", contactSchema);
