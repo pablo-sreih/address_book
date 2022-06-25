ab0a251199b2a884bf406f791b6f52d4f21470c7
Pablo Sreih <88791435+pablo-sreih@users.noreply.github.com>
2022-06-26 00:40:29 +0300

    Added Login Page

--- Frontend/addrssr/src/App.js
+++ Frontend/addrssr/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import LoginForm from './components/LoginForm';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
     <>
-    <LoginForm />
+    <LoginPage />
     </>
   );
 }
---
+++ Frontend/addrssr/src/pages/LoginPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import LoginForm from "../components/LoginForm"
+
+const Login = () => {
+    return(
+        <div className="BG">
+            <div className="login-container">
+                <img alt=""></img>
+                <LoginForm/>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
