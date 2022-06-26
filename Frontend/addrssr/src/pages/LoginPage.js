4a3d542587305d3d8465f55196787676039b4491
Pablo Sreih <88791435+pablo-sreih@users.noreply.github.com>
2022-06-26 08:48:01 +0300

    Added Logo to Login Page

--- Frontend/addrssr/src/pages/LoginPage.js
+++ Frontend/addrssr/src/pages/LoginPage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import LoginForm from "../components/LoginForm"
+import image from "../assets/images/logos/addrssr.png"
 
 const Login = () => {
     return(
         <div className="BG">
             <div className="login-container">
+                <img src={image}></img>
                 <LoginForm/>
             </div>
         </div>
