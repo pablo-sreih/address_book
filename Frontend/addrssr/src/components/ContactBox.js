d86c130e90aa88b876b6259a1175b7bf5efd38e6
Pablo Sreih <88791435+pablo-sreih@users.noreply.github.com>
2022-06-26 21:53:18 +0300

    Added Delete Button to Contact Box

--- Frontend/addrssr/src/components/ContactBox.js
+++ Frontend/addrssr/src/components/ContactBox.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLink, faEnvelope, faPhone,faLocation } from '@fortawesome/free-solid-svg-icons'
+import { faLink, faTrash, faEnvelope, faPhone,faLocation } from '@fortawesome/free-solid-svg-icons'
 
 
 
 function ContactBox(props) {
   return (
     <div className='contactbox'>
+        <div className='delete-button'><FontAwesomeIcon icon={faTrash} size="lg"/></div>
         <div className='contact-name'>{props.name}</div>
         <br></br>
         <div><FontAwesomeIcon icon={faEnvelope}/> {props.email}</div>
