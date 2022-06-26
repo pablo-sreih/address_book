0d68a9649d0d949b45a7d8c5f30e2bb889fb6708
Pablo Sreih <88791435+pablo-sreih@users.noreply.github.com>
2022-06-26 15:36:17 +0300

    Added Linking to Contacts Page

--- Frontend/addrssr/src/pages/ContactsPage.js
+++ Frontend/addrssr/src/pages/ContactsPage.js
@@ -1,16 +1,42 @@
 import React from 'react'
 import ContactBox from '../components/ContactBox'
 import NavBar from '../components/NavBar'
+import axios from 'axios'
+import qs from 'qs'
+import { useState, useEffect } from 'react'
 
 function ContactsPage() {
+
+    const [contacts, setContacts] = useState([])
+
+    function getContacts(){
+        const id = localStorage.getItem("id")
+
+        axios({
+            method: "POST",
+            headers: { 'content-type': 'application/x-www-form-urlencoded' },
+            url: "http://localhost:4001/get-contacts",
+            userid: qs.stringify(id),
+        })
+
+        .then(function (response) {
+            setContacts(response.data)
+            console.log(contacts)
+        })
+
+        .catch( (error)=> {
+            alert("No Contacts Found")
+        })
+    }
+
+    useEffect(()=>{
+        getContacts()
+    },[])
+
   return (
     <div>
         <NavBar/>
         <div className='contact-container'>
-            <ContactBox/>
-            <ContactBox/>
-            <ContactBox/>
-            <ContactBox/>
         </div>
     </div>
   )
